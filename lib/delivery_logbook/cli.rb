55cdcdf64dc857e1cec30ebf738e742c87921c18
Vincent Diehl <vinny.diehl@gmail.com>
2014-11-08 04:05:11 -0500

    Implement `dlog add`

--- lib/delivery_logbook/cli.rb
+++ lib/delivery_logbook/cli.rb
@@ -1,4 +1,4 @@
-%w[logbook version].each { |r| require "delivery_logbook/#{r}" }
+%w[flags logbook order version].each { |r| require "delivery_logbook/#{r}" }
 
 require "commander/import"
 require "date"
@@ -20,11 +20,39 @@ module DeliveryLogbook
       command :add do |c|
         c.syntax = "dlog add [options]"
         c.description = "Add deliveries."
-        c.option "-d DATE", Date,
+        c.option "--date DATE", String,
                  "Choose the date to add deliveries for (default today)"
 
         c.action do |args, opts|
-          opts.default date: Date.today
+          # Date is an unsupported option type so do it with strings
+          date = Date.parse opts.date || Date.today.to_s
+
+          loop do
+            # String inputs get #to_s because of a nasty HighLine bug that
+            # causes HighLine::String YAML serialization to be irreversible
+
+            ticket = ask("Ticket (q to quit): ") do |q|
+              q.validate = /(q(uit)?|\d{8}#{FLAGS_ANY_REGEX})/i
+            end.to_s
+
+            break if ticket[0] == "q"
+
+            # Ticket number and flags are input at once, break them down into
+            # two separate variables
+            flags = (ticket[FLAGS_REGEX] || "").downcase.chars.map &:to_sym
+            ticket.gsub! FLAGS_REGEX, ""
+
+            total = ask("Total: ", Float) { |q| q.above = 0 }
+            received = ask("Received: ", Float) { |q| q.above = 0 }
+
+            notes = ask("Notes: ").to_s
+
+            order = Order.new ticket, total, received, date, notes, flags
+
+            puts
+
+            log.add order
+          end
         end
       end
 
