d6183fe1cd727c1ef1b3182de1de687dafd47e13
Vincent Diehl <vinny.diehl@gmail.com>
2014-11-12 19:05:31 -0500

    Revise flags

--- lib/delivery_logbook/flags.rb
+++ lib/delivery_logbook/flags.rb
@@ -3,12 +3,14 @@ module DeliveryLogbook
     # Anything less than a dollar is a stiff, good is $5 or more
     s: "Stiff",
     t: "Good Tip",
-    # Fuckers that send their kids to the door so they don't have to tip
-    # If you do this you deserve to have a thin stream of acid drizzled onto
-    # your ballsack
-    k: "Kids",
+
+    # At the door by the time I got to it
+    p: "Prompt",
     # Took a long time to come to the door
     l: "Long",
+
+    # Kids can order from any address and tend not to tip so flag them
+    k: "Kids"
   }
 
   FLAGS_REGEX, FLAGS_ANY_REGEX = %w[+ *].map { |r| /[#{FLAGS.keys.join}]#{r}/i }
