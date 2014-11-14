44d6aead9b532fd9130f65f66ebe0c69fa53912e
Vincent Diehl <vinny.diehl@gmail.com>
2014-11-13 23:58:40 -0500

    Refactor Logbook
    Making everything static, having to instantiate Logbook felt dirty.

--- lib/delivery_logbook/logbook.rb
+++ lib/delivery_logbook/logbook.rb
@@ -1,4 +1,5 @@
 require "delivery_logbook/data"
+require "delivery_logbook/order"
 
 require "date"
 require "nutella"
@@ -7,49 +8,51 @@ require "yaml"
 
 module DeliveryLogbook
   class Logbook
-    def initialize
-      @orders = File.exist?(DATA_FILE) ? YAML.load(File.read DATA_FILE) : []
-    end
+    class << self
+      def add(order)
+        save orders << order
+      end
 
-    def add(order)
-      @orders << order
-      save
-    end
+      def delete(ticket)
+        save orders.delete_if { |o| o.ticket == ticket }
+      end
 
-    def delete(ticket)
-      @orders.delete_if { |o| o.ticket == ticket }
-      save
-    end
-
-    def search(query, method)
-      @orders.select do |order|
-        case method
-        when :date
-          order.date == Date.parse(query)
-        when :address
-          # TODO: Search entire streets (also looser address search in general)
-          order.address == StreetAddress::US.parse(query)
-        when :notes
-          # TODO: A smarter notes search
-          order.notes.downcase.include? query
-        when :flags
-          # The order should have all of the flags queried for
-          (query.chars.map(&:to_sym) - order.flags).empty?
-        else
-          # Anything else should be an exact match
-          order.send(method) == query
+      def search(query, method)
+        orders.select do |order|
+          case method
+          when :date
+            order.date == Date.parse(query)
+          when :address
+            # TODO: Search entire streets (also looser address search in general)
+            order.address == StreetAddress::US.parse(query)
+          when :notes
+            # TODO: A smarter notes search
+            order.notes.downcase.include? query
+          when :flags
+            # The order should have all of the flags queried for
+            (query.chars.map(&:to_sym) - order.flags).empty?
+          else
+            # Anything else should be an exact match
+            order.send(method) == query
+          end
         end
       end
-    end
 
-    def customer(address)
-    end
+      def customer(address)
+      end
 
-    private
+      private
 
-    # Saves the orders to a YAML file.
-    def save
-      File.write DATA_FILE, @orders.to_yaml
+      # Returns the orders in the data file (or an empty array if there are
+      # none)
+      def orders
+        File.exist?(DATA_FILE) ? YAML.load(File.read DATA_FILE) : []
+      end
+
+      # Saves +new_orders+ to a YAML file.
+      def save(new_orders)
+        File.write DATA_FILE, new_orders.to_yaml
+      end
     end
   end
 end
